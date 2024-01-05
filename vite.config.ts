2e4fc5964e91c1c8f3c2506fc4e1d62e16d090d2
Patrick Arminio <patrick.arminio@gmail.com>
2024-01-05 22:37:09 +0100

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import type { Plugin } from "vite";
 import { defineConfig } from "vite";
 import path from "path";
 import dts from "vite-plugin-dts";
@@ -7,17 +6,13 @@ const name = "index";
 
 export default defineConfig(() => {
   return {
+    plugins: [dts()],
     build: {
       lib: {
         entry: path.resolve(__dirname, "src/index.ts"),
-        name: "astroMetaTags",
+        name: "astro-meta-tags",
         fileName: (format) => (format === "es" ? `${name}.mjs` : `${name}.js`),
       },
     },
-    plugins: [
-      dts({
-        outDir: "dist/types",
-      }) as unknown as Plugin,
-    ],
   };
 });
